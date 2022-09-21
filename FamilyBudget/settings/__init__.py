02ca6f5c29ca1205c20c3b41fa308be35b8e4765
mswierkocki <marcin.swierkocki@gmail.com>
2022-09-21 05:22:10 +0200

    Formating

--- FamilyBudget/settings/__init__.py
+++ FamilyBudget/settings/__init__.py
@@ -1,11 +1,11 @@
-#import os
-# os.environ['APP_ENV']
-from .base import *
+# flake8: noqa
+
+from .base import *  
 from decouple import config
 APP_ENV = config('APP_ENV', default="dev", cast=str)
 if APP_ENV == 'prod':
-    from .prod import *
+    from .prod import *  
 elif APP_ENV == 'heroku':
-    from .heroku import *
+    from .heroku import * 
 else:
     from .dev import *
