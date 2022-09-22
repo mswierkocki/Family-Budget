c8093709ebeddf4ce08ad9401fce72137d32b64d
mswierkocki <marcin.swierkocki@gmail.com>
2022-09-22 16:19:53 +0200

    Add permission for profile required

--- budget_app/views_api.py
+++ budget_app/views_api.py
@@ -1,14 +1,21 @@
 # REST framework
+from urllib import request
 from rest_framework import viewsets
 # from rest_framework.views import APIView
 from rest_framework import filters
 
 
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import BasePermission, IsAdminUser
 
-# from .serializers import BudgetSerializer
+from .serializers import BudgetSerializer
 
+from budget_app.models import Budget
 from budget_app.filters import IsOwnerFilterBackend
+
+
+class ProfileRequiredPermisson(BasePermission):
+    def has_permission(self, request, view):
+        return hasattr(request.user, 'profile')
 
 
 class BudgetAPI(viewsets.ModelViewSet):
@@ -16,8 +23,16 @@ class BudgetAPI(viewsets.ModelViewSet):
     Budget Api
     """
 
-    permission_classes = [IsAdminUser]
+    permission_classes = [ProfileRequiredPermisson, IsAdminUser]
     filter_backends = [IsOwnerFilterBackend,
                        filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['name', 'shared']
     search_fields = ['^name', ]
+
+    def get_queryset(self):
+        return Budget.objects.filter(owner=self.request.user.profile)
+
+    def get_serializer_class(self):
+        # if self.action == 'create':
+
+        return BudgetSerializer
