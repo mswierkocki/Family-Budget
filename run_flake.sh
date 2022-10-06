a7e376cc37460489ada64139622a9fe3272f9cf2
mswierkocki <marcin.swierkocki@gmail.com>
2022-10-06 15:15:44 +0200

    Update workflow and run

--- run_flake.sh
+++ run_flake.sh
@@ -4,6 +4,7 @@ EXITS_ZERO="--exit-zero"
 MAX_LINE="127"
 if [ -z "$1" ]
   then
+    echo "Run  argument is null"
     EXITS_ZERO=""
 fi
 flake8 . --exclude .git,.venv,*migrations* --count --show-source --statistics --max-line-length=$MAX_LINE $EXITS_ZERO
